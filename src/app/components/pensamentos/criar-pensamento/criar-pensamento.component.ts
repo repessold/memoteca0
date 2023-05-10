eeab06c0947de019dee672fe3612b1c281bcbfc1
repessold <rafaela.casrep@gmail.com>
2023-05-10 11:45:44 -0300

    event binding

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  constructor() {
-    
+  pensamento={
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo:'',
   }
 
+  constructor() { }
+
   ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    alert("Novo pensamento criado!")
+  }
+
+  cancelar(){
+    alert("Pensamento cancelado.")
   }
 
 }
