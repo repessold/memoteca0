edbd1ea4048a7c16b664518b0d059b271ba09b00
repessold <rafaela.casrep@gmail.com>
2023-05-11 11:47:12 -0300

    tipos de diretivas

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,6 +17,11 @@ export class ListarPensamentoComponent implements OnInit {
     conteudo: 'Minha propriedade é decorada com @Input',
     autoria: 'Componente filho',
     modelo: 'modelo1'
+  },
+  {
+    conteudo: 'EXEMPLO',
+    autoria: 'EXEMPLO',
+    modelo: 'modelo1'
   }];
 
   constructor() { }
